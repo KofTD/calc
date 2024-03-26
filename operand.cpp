25e5e626bbe820ef57d6b515a945938e064097de
KofTD <KOfTD@yandex.ru>
2024-03-26 23:43:22 +0300

    4st commit

--- operand.cpp
+++ operand.cpp
@@ -1,159 +1,98 @@
 #include "operand.h"
-#include <map>
 
 namespace expression {
-    prefixes str_to_preffix(char preffix){
-        using enum expression::prefixes;
-        if (preffix == 'Q')
-            return quetta;
-        else if (preffix == 'R')
-            return ronna;
-        else if(preffix=='Y')
-            return yotta;
-        else if(preffix =='Z')
-            return zetta;
-        else if(preffix == 'E')
-            return exa;
-        else if(preffix == 'P')
-            return peta;
-        else if(preffix == 'T')
-            return tera;
-        else if(preffix == 'G')
-            return giga;
-        else if(preffix == 'M')
-            return mega;
-        else if(preffix == 'k')
-            return kilo;
-        else if(preffix =='h')
-            return hecto;
-        else if(preffix == 'd')
-            return deci;
-        else if(preffix == 'c')
-            return centi;
-        else if(preffix == 'm')
-            return milli;
-        else if(preffix == 'n')
-            return nano;
-        else if(preffix == 'p')
-            return pico;
-        else if(preffix == 'f')
-            return femto;
-        else if(preffix == 'a')
-            return atto;
-        else if(preffix == 'z')
-            return zepto;
-        else if(preffix == 'y')
-            return yocto;
-        else if(preffix == 'r')
-            return ronto;
-        else if(preffix == 'q')
-            return quecto;
-        else
-            throw std::exception();
+int8_t str_to_prefix(const std::string &prefix) {
+  if (!prefixes.contains(prefix)) {
+    throw std::exception();
+  }
+
+  return prefixes.at(prefix);
+}
+
+std::pair<int8_t, std::string>
+split_prefix_and_unit(const std::string &prefix_and_unit) {
+  std::pair<int8_t, std::string> result;
+  const std::string prefixes_string = "QRYZEPTGMkhdcmnpfazyrq";
+
+  if (prefix_and_unit.length() >= 2) {
+    auto sample = prefix_and_unit.substr(0, 3);
+
+    if (sample == "mic") {
+      result.first = prefixes.at("mic");
+      result.second = prefix_and_unit.substr(3);
+    } else if (sample.substr(0, 2) == "da") {
+      result.first = prefixes.at("de");
+      result.second = prefix_and_unit.substr(2);
+    } else if (prefixes.contains(sample.substr(0, 1))) {
+      result.first = str_to_prefix(sample.substr(0, 1));
+      result.second = prefix_and_unit.substr(1);
+    } else {
+      result.first = 0;
+      result.second = prefix_and_unit;
     }
+  } else {
+    result.first = 0;
+    result.second = prefix_and_unit;
+  }
 
+  return result;
+}
+std::array<int8_t, n_of_base_units> convert_to_basic_units(
+    const std::string
+        &unit) { // NOLINT(readability-function-cognitive-complexity)
+  // s, m, g, A, K, mol, cd
+  if (!physical_units.contains(unit)) {
+    throw std::exception();
+  }
 
-    std::pair<prefixes, std::string> split_prefix_and_unit(const std::string& prefix_and_unit) {
-        std::pair<prefixes, std::string> result;
-        const std::string prefixes_string = "QRYZEPTGMkhdcmnpfazyrq";
+  return physical_units.at(unit);
+}
+std::array<int8_t, n_of_base_units>
+dif_b_u_powers(std::array<int8_t, n_of_base_units> lhs,
+               std::array<int8_t, n_of_base_units> rhs) {
+  std::array<int8_t, n_of_base_units> result = {0, 0, 0, 0, 0, 0, 0};
 
-        if (prefix_and_unit.length() >= 2) {
-            auto sample = prefix_and_unit.substr(0, 3);
+  for (size_t i = 0; i < lhs.size(); ++i) {
+    result.at(i) = static_cast<int8_t>(lhs.at(i) - rhs.at(i));
+  }
 
-            if (sample == "mic") {
-                result.first = prefixes::micro;
-                result.second = prefix_and_unit.substr(3);
-            } else if (sample.substr(0, 2) == "da") {
-                result.first = prefixes::deca;
-                result.second = prefix_and_unit.substr(2);
-            } else if(prefixes_string.find(sample[0]) != std::string::npos){
-                result.first = str_to_preffix(sample[0]);
-                result.second = prefix_and_unit.substr(1);
-            }else{
-                result.first = prefixes::null;
-                result.second = prefix_and_unit;
-            }
-        } else {
-            result.first = prefixes::null;
-            result.second = prefix_and_unit;
-        }
+  return result;
+}
+std::array<int8_t, n_of_base_units>
+sum_b_u_powers(std::array<int8_t, n_of_base_units> lhs,
+               std::array<int8_t, n_of_base_units> rhs) {
+  std::array<int8_t, n_of_base_units> result = {0, 0, 0, 0, 0, 0, 0};
 
-        return result;
+  for (size_t i = 0; i < lhs.size(); ++i) {
+    result.at(i) = static_cast<int8_t>(lhs.at(i) + rhs.at(i));
+  }
+
+  return result;
+}
+bool is_equal_powers(std::array<int8_t, n_of_base_units> lhs,
+                     std::array<int8_t, n_of_base_units> rhs) {
+  for (size_t i = 0; i < lhs.size(); i++) {
+    if (lhs.at(i) != rhs.at(i)) {
+      return false;
     }
-    std::array<int8_t, 7> convert_to_basic_units(const std::string &unit) {
-        // s, m, g, A, K, mol, cd
-        if (unit == "s")
-            return {1, 0, 0, 0, 0, 0, 0};
-        else if (unit == "m")
-            return {0, 1, 0, 0, 0, 0, 0};
-        else if (unit == "g")
-            return {0, 0, 1, 0, 0, 0, 0};
-        else if (unit == "A")
-            return {0, 0, 0, 1, 0, 0, 0};
-        else if (unit == "K")
-            return {0, 0, 0, 0, 1, 0, 0};
-        else if (unit == "mol")
-            return {0, 0, 0, 0, 0, 1, 0};
-        else if (unit == "cd" || unit == "lm")
-            return {0, 0, 0, 0, 0, 0, 1};
-        else if (unit == "Hz" || unit == "Bq")
-            return {-1, 0, 0, 0, 0, 0, 0};
-        else if (unit == "N")
-            return {-2, 1, 3, 0, 0, 0, 0};
-        else if (unit == "Pa")
-            return {-2, -1, 3, 0, 0, 0, 0};
-        else if (unit == "J")
-            return {-2, 2, 3, 0, 0, 0, 0};
-        else if (unit == "W")
-            return {-3, 2, 3, 0, 0, 0, 0};
-        else if (unit == "C")
-            return {1, 0, 0, 1, 0, 0, 0};
-        else if (unit == "V")
-            return {-3, 2, 3, -1, 0, 0, 0};
-        else if (unit == "F")
-            return {4, -2, -3, 2, 0, 0, 0};
-        else if (unit == "ohm")
-            return {-3, 2, 3, -2, 0, 0, 0};
-        else if (unit == "S")
-            return {3, -2, -3, 2, 0, 0, 0};
-        else if (unit == "Wb")
-            return {-2, 2, 3, -1, 0, 0, 0};
-        else if (unit == "T")
-            return {-2, 0, 3, -1, 0, 0, 0};
-        else if (unit == "H")
-            return {-2, 2, 3, -2, 0, 0, 0};
-        else if (unit == "lx")
-            return {0, -2, 0, 0, 0, 0, 1};
-        else if (unit == "Gy" || unit == "Sv")
-            return {-2, 2, 0, 0, 0, 0, 0};
-        else if (unit == "kat")
-            return {-1, 0, 0, 0, 0, 1, 0};
-        else
-            throw std::exception();
-    }
-    std::array<int8_t, 7> dif_b_u_powers(std::array<int8_t, 7> lhs, std::array<int8_t, 7> rhs) {
-        std::array<int8_t, 7> result = {0, 0, 0, 0, 0, 0, 0};
+  }
+  return true;
+}
 
-        for (size_t i = 0; i < lhs.size(); ++i) {
-            result[i] = static_cast<int8_t>(lhs[i] - rhs[i]);
-        }
-
-        return result;
+double ten_to_pow(int8_t power) {
+  if (power == 0) {
+    return 1;
+  }
+  double result = 1;
+  if (power > 0) {
+    for (int8_t _ = 0; _ < power; _++) {
+      result *= 10; // NOLINT
     }
-    std::array<int8_t, 7> sum_b_u_powers(std::array<int8_t, 7> lhs, std::array<int8_t, 7> rhs) {
-        std::array<int8_t, 7> result = {0, 0, 0, 0, 0, 0, 0};
-
-        for (size_t i = 0; i < lhs.size(); ++i) {
-            result[i] = static_cast<int8_t>(lhs[i] + rhs[i]);
-        }
-
-        return result;
+  } else {
+    for (int8_t _ = 0; _ < power; _++) {
+      result /= 10; // NOLINT
     }
-    bool is_equal_powers(std::array<int8_t, 7> lhs, std::array<int8_t, 7> rhs) {
-        for (size_t i = 0; i < lhs.size(); i++) {
-            if (lhs[i] != rhs[i])
-                return false;
-        }
-        return true;
-    }
-}// namespace expression
+  }
+  return result;
+}
+} // namespace expression
--- operand.h
+++ operand.h
@@ -1,141 +1,204 @@
 #ifndef CALCULATOR_OPERAND_H
 #define CALCULATOR_OPERAND_H
 
+#include <algorithm>
 #include <array>
 #include <cmath>
 #include <cstdint>
 #include <map>
 #include <ostream>
+#include <ranges>
 #include <string>
 
 namespace expression {
-    using std::int8_t;
+using std::int8_t;
 
-    enum class prefixes : int8_t {
-        quetta = 30,
-        ronna = 27,
-        yotta = 24,
-        zetta = 21,
-        exa = 18,
-        peta = 15,
-        tera = 12,
-        giga = 9,
-        mega = 6,
-        kilo = 3,
-        hecto = 2,
-        deca = 1,
-        null = 0,
-        deci = -1,
-        centi = -2,
-        milli = -3,
-        micro = -6,
-        nano = -9,
-        pico = -12,
-        femto = -15,
-        atto = -18,
-        zepto = -21,
-        yocto = -24,
-        ronto = -27,
-        quecto = -30
-    };
+constexpr int8_t n_of_base_units = 7;
 
-    std::pair<prefixes, std::string> split_prefix_and_unit(const std::string &prefix_and_unit);
+const std::map<std::string, int8_t> prefixes = {
+    {"Q", 30},  {"R", 27},  {"Y", 24},   {"Z", 21},  {"E", 18},  {"P", 15},
+    {"T", 12},  {"G", 9},   {"M", 6},    {"k", 3},   {"da", 1},  {"d", -1},
+    {"c", -2},  {"m", -3},  {"mic", -6}, {"n", -9},  {"p", -12}, {"f", -15},
+    {"a", -18}, {"z", -21}, {"y", -24},  {"r", -27}, {"q", -30}};
 
-    std::array<int8_t, 7> convert_to_basic_units(const std::string &unit);
+// enum class prefixes : int8_t {
+//   quetta = 30,
+//   ronna = 27,
+//   yotta = 24,
+//   zetta = 21,
+//   exa = 18,
+//   peta = 15,
+//   tera = 12,
+//   giga = 9,
+//   mega = 6,
+//   kilo = 3,
+//   hecto = 2,
+//   deca = 1,
+//   null = 0,
+//   deci = -1,
+//   centi = -2,
+//   milli = -3,
+//   micro = -6,
+//   nano = -9,
+//   pico = -12,
+//   femto = -15,
+//   atto = -18,
+//   zepto = -21,
+//   yocto = -24,
+//   ronto = -27,
+//   quecto = -30
+// };
 
-    bool is_equal_powers(std::array<int8_t, 7> lhs, std::array<int8_t, 7> rhs);
+const std::map<std::string, std::array<int8_t, n_of_base_units>>
+    physical_units = {
+        {"s", {1, 0, 0, 0, 0, 0, 0}},   {"m", {0, 1, 0, 0, 0, 0, 0}},
+        {"g", {0, 0, 1, 0, 0, 0, 0}},   {"A", {0, 0, 0, 1, 0, 0, 0}},
+        {"K", {0, 0, 0, 0, 1, 0, 0}},   {"mol", {0, 0, 0, 0, 0, 1, 0}},
+        {"cd", {0, 0, 0, 0, 0, 0, 1}},  {"lm", {0, 0, 0, 0, 0, 0, 1}},
+        {"Hz", {-1, 0, 0, 0, 0, 0, 0}}, {"Bq", {-1, 0, 0, 0, 0, 0, 0}},
+        {"N", {-2, 1, 3, 0, 0, 0, 0}},  {"Pa", {-2, -1, 3, 0, 0, 0, 0}},
+        {"J", {-2, 2, 3, 0, 0, 0, 0}},  {"W", {-3, 2, 3, 0, 0, 0, 0}},
+        {"C", {1, 0, 0, 1, 0, 0, 0}},   {"V", {-3, 2, 3, -1, 0, 0, 0}},
+        {"F", {4, -2, -3, 2, 0, 0, 0}}, {"ohm", {-3, 2, 3, -2, 0, 0, 0}},
+        {"S", {3, -2, -3, 2, 0, 0, 0}}, {"Wb", {-2, 2, 3, -1, 0, 0, 0}},
+        {"T", {-2, 0, 3, -1, 0, 0, 0}}, {"H", {-2, 2, 3, -2, 0, 0, 0}},
+        {"lx", {0, -2, 0, 0, 0, 0, 1}}, {"Gy", {-2, 2, 0, 0, 0, 0, 0}},
+        {"Sv", {-2, 2, 0, 0, 0, 0, 0}}, {"kat", {-1, 0, 0, 0, 0, 1, 0}}};
 
-    std::array<int8_t, 7> sum_b_u_powers(std::array<int8_t, 7> lhs, std::array<int8_t, 7> rhs);
+double ten_to_pow(int8_t power);
 
-    std::array<int8_t, 7> dif_b_u_powers(std::array<int8_t, 7> lhs, std::array<int8_t, 7> rhs);
+std::pair<int8_t, std::string>
+split_prefix_and_unit(const std::string &prefix_and_unit);
 
-    class operand {
-    public:
-        explicit operand(const std::string &number) {
-            this->number = std::stod(number);
-        }
-        operand(const std::string &number, const std::string &unit) {
-            this->number = std::stod(number);
+std::array<int8_t, n_of_base_units>
+convert_to_basic_units(const std::string &unit);
 
-            auto prefix_and_unit = split_prefix_and_unit(unit);
+bool is_equal_powers(std::array<int8_t, n_of_base_units> lhs,
+                     std::array<int8_t, n_of_base_units> rhs);
 
-            this->prefix = prefix_and_unit.first;
-            this->basic_units_powers = convert_to_basic_units(prefix_and_unit.second);
-        }
+std::array<int8_t, n_of_base_units>
+sum_b_u_powers(std::array<int8_t, n_of_base_units> lhs,
+               std::array<int8_t, n_of_base_units> rhs);
 
-        operand operator+(const operand &rhs) const {
-            if (!is_equal_powers(this->basic_units_powers, rhs.basic_units_powers))
-                throw std::exception();
+std::array<int8_t, n_of_base_units>
+dif_b_u_powers(std::array<int8_t, n_of_base_units> lhs,
+               std::array<int8_t, n_of_base_units> rhs);
 
-            double n_number = (this->number * int8_t(this->prefix)) + (rhs.number * int8_t(rhs.prefix));
-            prefixes n_prefix = std::min(this->prefix, rhs.prefix);
+class operand {
+public:
+  explicit operand(const std::string &number) : number(std::stod(number)) {}
 
-            if (static_cast<int8_t>(n_prefix) != 0)
-                n_number /= static_cast<double>(n_prefix);
+  operand(const std::string &number, const std::string &unit)
+      : number(
+            std::stod(number)) { // NOLINT(bugprone-easily-swappable-parameters)
+    auto prefix_and_unit = split_prefix_and_unit(unit);
 
-            return {n_number, n_prefix, this->basic_units_powers};
-        }
-        operand operator-(const operand &rhs) const {
-            if (!is_equal_powers(this->basic_units_powers, rhs.basic_units_powers))
-                throw std::exception();
+    this->prefix = prefix_and_unit.first;
+    this->basic_units_powers = convert_to_basic_units(prefix_and_unit.second);
+  }
 
-            double n_number = (this->number * int8_t(this->prefix)) - (rhs.number * int8_t(rhs.prefix));
-            prefixes n_prefix = std::min(this->prefix, rhs.prefix);
+  operand operator+(const operand &rhs) const {
+    if (!is_equal_powers(this->basic_units_powers, rhs.basic_units_powers)) {
+      throw std::exception();
+    }
 
-            if (static_cast<int8_t>(n_prefix) != 0)
-                n_number /= static_cast<double>(n_prefix);
+    double n_number =
+        (this->number * ten_to_pow(static_cast<int8_t>(this->prefix))) +
+        (rhs.number * ten_to_pow(static_cast<int8_t>(rhs.prefix)));
+    int8_t n_prefix = std::min(this->prefix, rhs.prefix);
 
-            return {n_number, n_prefix, this->basic_units_powers};
-        }
-        operand operator*(const operand &rhs) const {
-            double n_number = this->number * int8_t(this->prefix) * rhs.number * int8_t(rhs.prefix);
-            prefixes n_prefix = std::min(this->prefix, rhs.prefix);
+    if (static_cast<int8_t>(n_prefix) != 0) {
+      n_number /= static_cast<double>(n_prefix);
+    }
 
-            return {n_number, n_prefix, sum_b_u_powers(this->basic_units_powers, rhs.basic_units_powers)};
-        }
-        operand operator/(const operand &rhs) const {
-            double n_number = this->number * int8_t(this->prefix) / rhs.number * int8_t(rhs.prefix);
-            prefixes n_prefix = std::min(this->prefix, rhs.prefix);
+    return {n_number, n_prefix, this->basic_units_powers};
+  }
+  operand operator-(const operand &rhs) const {
+    if (!is_equal_powers(this->basic_units_powers, rhs.basic_units_powers)) {
+      throw std::exception();
+    }
 
-            return {n_number, n_prefix, dif_b_u_powers(this->basic_units_powers, rhs.basic_units_powers)};
-        }
+    double n_number =
+        (this->number * ten_to_pow(static_cast<int8_t>(this->prefix))) -
+        (rhs.number * ten_to_pow(static_cast<int8_t>(rhs.prefix)));
+    int8_t n_prefix = std::min(this->prefix, rhs.prefix);
 
-        [[nodiscard]] operand power(const operand &rhs) const {
-            if (!is_equal_powers(rhs.basic_units_powers, {0, 0, 0, 0, 0, 0, 0}))
-                throw std::exception();
+    if (static_cast<int8_t>(n_prefix) != 0) {
+      n_number /= static_cast<double>(n_prefix);
+    }
 
-            double n_number = std::pow(this->number, rhs.number);
+    return {n_number, n_prefix, this->basic_units_powers};
+  }
+  operand operator*(const operand &rhs) const {
+    double n_number = this->number *
+                      ten_to_pow(static_cast<int8_t>(this->prefix)) *
+                      rhs.number * ten_to_pow(static_cast<int8_t>(rhs.prefix));
+    int8_t n_prefix = std::min(this->prefix, rhs.prefix);
 
-            std::array<int8_t, 7> n_powers = {0, 0, 0, 0, 0, 0, 0};
+    return {n_number, n_prefix,
+            sum_b_u_powers(this->basic_units_powers, rhs.basic_units_powers)};
+  }
+  operand operator/(const operand &rhs) const {
+    double n_number = this->number *
+                      ten_to_pow(static_cast<int8_t>(this->prefix)) /
+                      rhs.number * ten_to_pow(static_cast<int8_t>(rhs.prefix));
+    int8_t n_prefix = std::min(this->prefix, rhs.prefix);
 
-            for (size_t i = 0; i < n_powers.size(); ++i) {
-                n_powers[i] = static_cast<int8_t>(basic_units_powers[i] * static_cast<int8_t>(rhs.number));
-            }
+    return {n_number, n_prefix,
+            dif_b_u_powers(this->basic_units_powers, rhs.basic_units_powers)};
+  }
 
-            return {n_number, this->prefix, n_powers};
-        }
+  [[nodiscard]] operand power(const operand &rhs) const {
+    if (!is_equal_powers(rhs.basic_units_powers, {0, 0, 0, 0, 0, 0, 0})) {
+      throw std::exception();
+    }
 
-        friend std::ostream &operator<<(std::ostream &out, const operand &op) {
-            out << op.number;
+    double n_number = std::pow(this->number, rhs.number);
 
-            return out;
-        }
+    std::array<int8_t, n_of_base_units> n_powers = {0, 0, 0, 0, 0, 0, 0};
 
+    for (size_t i = 0; i < n_powers.size(); ++i) {
+      n_powers.at(i) = static_cast<int8_t>(basic_units_powers.at(i) *
+                                           static_cast<int8_t>(rhs.number));
+    }
 
-    protected:
-        operand(double number, prefixes prefix, std::array<int8_t, 7> b_u_powers) {
-            this->number = number;
-            this->prefix = prefix;
-            this->basic_units_powers = b_u_powers;
-        }
+    return {n_number, this->prefix, n_powers};
+  }
 
-    private:
-        double number;
-        prefixes prefix = prefixes::null;
-        std::array<int8_t, 7> basic_units_powers = {0, 0, 0, 0, 0, 0, 0};// s, m, g, A, K, mol, cd
-    };
+  friend std::ostream &operator<<(std::ostream &out, const operand &oper) {
+    using std::ranges::views::keys;
 
+    out << oper.number;
 
-}// namespace expression
+    for (const auto &[prefix, prefix_power] : prefixes) {
+      if (prefix_power == oper.prefix) {
+        out << prefix;
+        break;
+      }
+    }
 
-#endif//CALCULATOR_OPERAND_H
+    for (const auto &[unit, unit_powers] : physical_units) {
+      if (is_equal_powers(unit_powers, oper.basic_units_powers)) {
+        out << unit;
+        break;
+      }
+    }
+
+    return out;
+  }
+
+protected:
+  operand(double number, int8_t prefix,
+          std::array<int8_t, n_of_base_units> b_u_powers)
+      : number(number), prefix(prefix), basic_units_powers(b_u_powers) {}
+
+private:
+  double number;
+  int8_t prefix = 0;
+  std::array<int8_t, n_of_base_units> basic_units_powers = {
+      0, 0, 0, 0, 0, 0, 0}; // s, m, g, A, K, mol, cd
+};
+
+} // namespace expression
+
+#endif // CALCULATOR_OPERAND_H
